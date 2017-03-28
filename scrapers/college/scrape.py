4b36737d87be73a31fd13b836783f41fda852375
Sophie Ota <sophieota@Sophies-MacBook-Air.local>
2017-03-28 17:37:34 -0400

    College data that actually works now

--- scrapers/college/scrape.py
+++ scrapers/college/scrape.py
@@ -9,19 +9,19 @@ years = ['2015-2016', '2014-2015', '2013-2014', '2012-2013', '2011-2012', '2010-
 list_of_rows = []
 
 for year in years:
-	print year
-	response = requests.get("https://columbian.gwu.edu/" + year)
-	html = response.content
+    print year
+    response = requests.get("https://columbian.gwu.edu/" + year)
+    html = response.content
 
-	soup = BeautifulSoup(html)
-	table = soup.find('table')
+    soup = BeautifulSoup(html)
+    table = soup.find('table')
 
-	for row in table.findAll('tr')[1:]:
-		list_of_cells = []
-    	list_of_cells.append(year)
-    	for cell in row.findAll('td'):
-    		list_of_cells.append(cell.text.encode('utf-8'))
-    	list_of_rows.append(list_of_cells)
+    for row in table.findAll('tr')[1:]:
+        list_of_cells = []
+        list_of_cells.append(year)
+        for cell in row.findAll('td'):
+            list_of_cells.append(cell.text.encode('utf-8'))
+        list_of_rows.append(list_of_cells)
 
 outfile = open("college.csv", "wb")
 writer = csv.writer(outfile)
