c0ed7314a37c8f2bae6a97b6b89072acba62e259
Sophie Ota <sophieota@Sophies-MacBook-Air.local>
2017-03-28 12:10:24 -0400

    Updated assignment for 3/28

--- scrapers/college/scrape.py
+++ scrapers/college/scrape.py
@@ -5,26 +5,25 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-year = ['2015-2016', '2014-2015', '2013-2014', '2012-2013', '2011-2012', '2010-2011']
-urlp1 = "https://columbian.gwu.edu/"
-url = urlp1 + str(year)
-response = requests.get(url)
-html = response.content
-
-for x in year.findAll('table')
-
-soup = BeautifulSoup(html)
-table = soup.find('table')
-
+years = ['2015-2016', '2014-2015', '2013-2014', '2012-2013', '2011-2012', '2010-2011']
 list_of_rows = []
-for row in table.findAll('tr')[1:-1]:
-    list_of_cells = []
-    for cell in row.findAll('td'):
-        text = cell.text
-        list_of_cells.append(text)
-    list_of_rows.append(list_of_cells)
+
+for year in years:
+	print year
+	response = requests.get("https://columbian.gwu.edu/" + year)
+	html = response.content
+
+	soup = BeautifulSoup(html)
+	table = soup.find('table')
+
+	for row in table.findAll('tr')[1:]:
+		list_of_cells = []
+    	list_of_cells.append(year)
+    	for cell in row.findAll('td'):
+    		list_of_cells.append(cell.text.encode('utf-8'))
+    	list_of_rows.append(list_of_cells)
 
 outfile = open("college.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerow(["department", "faculty", "sponsor", "title"])
+writer.writerow(["year", "department", "faculty", "sponsor", "title"])
 writer.writerows(list_of_rows)
